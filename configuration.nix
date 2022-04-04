1adef02578aea24aa71b08d9ea001a7e552098dd
Thomas Frank <thomas@franks-im-web.de>
2022-04-04 19:28:25 +0200

    added vbox

--- configuration.nix
+++ configuration.nix
@@ -86,6 +86,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     firefox
+    gnomeExtensions.dash-to-dock
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -123,5 +124,7 @@
   	plugins = [ "git" "python" "man" ];
   	theme = "agnoster";
   };
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "thomas" ];
 }
 
