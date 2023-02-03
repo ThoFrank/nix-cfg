0bff6e1ab5c5171b9f4ead392d689869af390d5c
Thomas Frank <thomas@franks-im-web.de>
2023-02-03 16:49:13 +0100

    update inputs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    unstable.url = "github:nixos/nixpkgs-channels/nixos-unstable";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
