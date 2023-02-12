f79fcbc280cfbe01d5160bea424a890cf7ac7581
Thomas Frank <thomas@franks-im-web.de>
2023-02-12 21:23:28 +0100

    ddclient for brebeck.online

--- includes/services/ddclient.nix
+++ includes/services/ddclient.nix
@@ -10,4 +10,22 @@
     passwordFile = "/home/thomas/src/nix-stuff/includes/services/password";
     domains = [ "tfrank.spdns.org" "frankcloud.firewall-gateway.com" ];
   };
+  
+  # container for brebeck.online
+  containers."brebeckOnline" = {
+    autoStart = true;
+    ephemeral = true;
+    config = {pkgs, ...}: {
+      system.stateVersion = "22.11";
+      services.ddclient = {
+        enable = true;
+        protocol = "namecheap";
+        use = "web, web=dynamicdns.park-your-domain.com/getip";
+        server = "dynamicdns.park-your-domain.com";
+        username = "brebeck.online";
+        passwordFile = "/.secret/brebeck.ddclient.pass";
+        domains = [ "brebeck.online" ];
+      };
+    };
+  };
 }
