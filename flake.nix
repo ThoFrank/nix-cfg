da1459b49b965ec291a46798c10016026045f863
Thomas Frank <thomas@franks-im-web.de>
2023-02-17 15:37:57 +0100

    added nixos-harware

--- flake.nix
+++ flake.nix
@@ -7,9 +7,11 @@
 
     home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, nixos-hardware, ... }: {
     overlays = {
       addUnstable = (
         final: prev: {
@@ -21,6 +23,8 @@
        system = "x86_64-linux";
       modules = [
         {nixpkgs.overlays = [self.overlays.addUnstable];}
+        nixos-hardware.nixosModules.common-cpu-intel
+        nixos-hardware.nixosModules.common-pc-ssd
         ./configuration.nix
         home-manager.nixosModules.home-manager
       ];
