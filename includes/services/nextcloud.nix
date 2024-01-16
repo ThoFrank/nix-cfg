7a38ad88b729db97ee0a2b525052032ddcbfe75a
Thomas Frank <thomas@franks-im-web.de>
2024-01-16 10:30:51 +0100

    upgrade to nextcloud 28

--- includes/services/nextcloud.nix
+++ includes/services/nextcloud.nix
@@ -3,7 +3,7 @@
   imports = [ ];
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud27;
+    package = pkgs.nextcloud28;
     hostName = "cloud.franks-im-web.de";
     https = true;
     home = "/mnt/tank/nextcloud/nextcloud";
