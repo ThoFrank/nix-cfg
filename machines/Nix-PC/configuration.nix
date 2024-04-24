29a456eeaad53aa6d5a2c2f6302dd19884326b6b
Thomas Frank <thomas@franks-im-web.de>
2024-04-24 15:10:04 +0200

    disable ssh password login

--- machines/Nix-PC/configuration.nix
+++ machines/Nix-PC/configuration.nix
@@ -142,7 +142,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.openFirewall = true;
-  services.openssh.settings.PasswordAuthentication = true;
+  services.openssh.settings.PasswordAuthentication = false;
 
   # services.httpd.enable = true;
   # services.httpd.adminAddr = "thomas@franks-im-web.de";
