60743a2b4f88edbe9054c813e22749cca04a44e1
Thomas Frank <thomas@franks-im-web.de>
2024-07-07 10:54:22 +0200

    mealie: move data

--- includes/services/mealie.nix
+++ includes/services/mealie.nix
@@ -2,6 +2,13 @@
 {
   services.mealie = {
     enable = true;
+    package = pkgs.unstable.mealie;
+  };
+
+  # move data to tank
+  fileSystems."/var/lib/private/mealie" = {
+    device = "/mnt/tank/services/mealie";
+    options = ["bind"];
   };
 
   networking.firewall.allowedTCPPorts = [
