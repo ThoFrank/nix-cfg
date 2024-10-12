b4d9f3c8a9cc3a14e4c063a7a2b36bc506fa63e5
Thomas Frank <thomas@franks-im-web.de>
2024-10-12 18:35:26 +0200

    nextcloud 29->30

--- includes/services/nextcloud.nix
+++ includes/services/nextcloud.nix
@@ -3,7 +3,7 @@
   imports = [ ];
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud29;
+    package = pkgs.nextcloud30;
     hostName = "cloud.franks-im-web.de";
     https = true;
     home = "/mnt/tank/nextcloud/nextcloud";
