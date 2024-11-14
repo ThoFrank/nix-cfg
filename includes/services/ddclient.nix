375babd2952a2f28faa2e3f2582f684472958510
Thomas Frank <thomas@franks-im-web.de>
2024-11-14 20:59:10 +0100

    switch from ddclient to inadyn

--- includes/services/ddclient.nix
+++ includes/services/ddclient.nix
@@ -1,13 +1,18 @@
 { config, pkgs, ... }:
 {
-  services.ddclient = {
+  services.inadyn = {
     enable = true;
-    protocol = "dyndns2";
-    use = "web, web=ipify-ipv4";
-    server = "update.spdyn.de";
-    ssl = true;
-    username = "seifar98@gmail.com";
-    passwordFile = "/home/thomas/src/nix-stuff/includes/services/password";
-    domains = [ "tfrank.spdns.org" "frankcloud.firewall-gateway.com" ];
+    settings.provider."default@spdyn.de" = {
+      ssl = true;
+      username = "seifar98@gmail.com";
+      hostname = "tfrank.spdns.org";
+      include = "/.secret/spdyn";
+    };
+    settings.provider."default@spdyn.de:2" = {
+      ssl = true;
+      username = "seifar98@gmail.com";
+      hostname = "frankcloud.firewall-gateway.com";
+      include = "/.secret/spdyn";
+    };
   };
 }
