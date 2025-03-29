b157609023b8c4ec3b23aefb103e71acd2d46de7
Thomas Frank <thomas@franks-im-web.de>
2025-03-29 14:00:16 +0100

    update to nextcloud 31

--- includes/services/nextcloud.nix
+++ includes/services/nextcloud.nix
@@ -3,7 +3,7 @@
   imports = [ ];
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud30;
+    package = pkgs.nextcloud31;
     hostName = "cloud.franks-im-web.de";
     https = true;
     # home = "/mnt/tank/nextcloud/nextcloud";
