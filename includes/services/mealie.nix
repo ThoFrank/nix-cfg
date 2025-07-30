cd35345852308bd4102846cd93cc674c6bf9abf9
Thomas Frank <thomas@franks-im-web.de>
2025-07-30 21:51:06 +0200

    update mealie

--- includes/services/mealie.nix
+++ includes/services/mealie.nix
@@ -1,18 +1,23 @@
 { config, pkgs, ... }:
-{
+let
+  url = "rezepte.franks-im-web.de";
+in {
   virtualisation.oci-containers.containers.mealie = {
-    image = "ghcr.io/mealie-recipes/mealie:v2.8.0";
+    image = "ghcr.io/mealie-recipes/mealie:v3.0.2";
     ports = ["127.0.0.1:${toString config.services.mealie.port}:${toString config.services.mealie.port}"];
     volumes = [
       "/mnt/tank/services/mealie/:/app/data/"
     ];
+    environment = {
+      BASE_URL = "https://${url}";
+    };
   };
 
   networking.firewall.allowedTCPPorts = [
     config.services.mealie.port
   ];
 
-  services.nginx.virtualHosts."rezepte.franks-im-web.de" = {
+  services.nginx.virtualHosts."${url}" = {
     forceSSL = true;
     enableACME = true;
     locations."/" = {
