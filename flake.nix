d9252f2bf5e3d7798b446e4359c6e08bb989f97b
Thomas Frank <thomas@franks-im-web.de>
2026-06-14 20:27:09 +0200

    Switching to 26.05

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "My setup";
   
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.11";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-26.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
